c6bbb043b7464d6a24391e49f8b89e6118b84659
VilkaDev <vilena.abdrakhimova@mail.ru>
2024-10-27 11:24:25 +0500

    Update RoleRepoImpl.java

--- PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepoImpl.java
+++ PP_3_1_2_Boot_Security/src/main/java/ru/kata/spring/boot_security/demo/repository/RoleRepoImpl.java
@@ -4,5 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import ru.kata.spring.boot_security.demo.model.Role;
 
+@Repository
 public interface RoleRepoImpl extends JpaRepository<Role, Long> {
 }
